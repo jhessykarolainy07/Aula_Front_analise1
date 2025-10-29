00a57a4c6a16ee2a13900d1e779ab9eca46616e8
jhessykarolainy07 <jhessykarolainy07@gmail.com>
2025-10-29 22:52:10 +0000

    atualizar objetos

---
+++ javascript/objetos.js
@@ -0,0 +1,24 @@
+// dados simples: numericos, strings, boolean...
+// array: [1,2,3, 'ana']
+// objetos: {nome: 'Luana', idade: 21}
+// funcões
+
+// Objetos literais 
+{
+    //propriedade: valor
+// key : value
+}
+
+const perfil = {
+    nome: 'Jhessy Monteiro
+    idade: 27,
+    profissao: "Assistente financeiro"
+    solteira: true
+
+}
+
+console.log(perfil.nome)
+console.log(perfil.idade)
+console.log(perfil.profissao)
+console.log(perfil.solteira)
+console.log(perfil.roupa)
