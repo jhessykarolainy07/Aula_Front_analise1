67dc3b400ec244161fccca6b0eead4aa9b0c90b8
jhessykarolainy07 <jhessykarolainy07@gmail.com>
2025-10-30 00:39:39 +0000

    atualizar função

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -9,11 +9,32 @@
 function dizOla(){
     alert("Diz olá!")
 }
-function dizOlpaPessoa(nome){
-    alert(´Não seja timido(a) ${nome}. Fala oi!´)
+function dizOlaPessoa(nome){
+    alert(`Não seja timido(a) ${nome}. Fala oi!`)
 }
+const jogadores = ['Neymar', 'Ronaldinho', 'Messi']
+const frutas = ['banana', 'morango', 'uva']
+
+function formataTexto(lista){
+    for(let texto of lista){
+        console.log(texto.toUpperCase())
+    //texto.toLowerCase() > tudo para minuscula
+    //texto.trim() > elimina espaços em branco
+    //texto.join(' outra coisa ') > junta com outro
+    }
+}
+// formataTexto(jogadores)
+// formataTexto(frutas)
+function adicao(n1, n2){
+    return n1 + n2
+}
+//faça uma versão para subtracao, multiplicacao, divisao
+
+const resultadoAdicao = adicao(5, 25) + 30
+console.log(`Adição: ${resultadoAdicao}`) // valor 30
+
 // Invocar ou chamar a função
-dizOla()
-dizOlaPessoa("Cleitinho")
-dizOlaPessoa("Ana")
-dizOlaPessoa("Julius")
+// dizOla()
+// dizOlaPessoa("Cleitinho")
+// dizOlaPessoa("Ana")
+// dizOlaPessoa("Julius")
